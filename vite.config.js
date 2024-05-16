65931f05f1dc6c037450c406bc10127084b484af
anand-vyas-gg <anand.vyas@globalgarner.com>
2024-05-16 19:31:41 +0530

    config setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import react from "@vitejs/plugin-react";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+// const { libInjectCss } = pkg;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    // build: {
+    //     lib: {
+    //         entry: path.resolve(__dirname, "src/index.jsx"),
+    //         name: "gg-bsw",
+    //         fileName: (format) => `gg-bsw.${format}.js`,
+    //     },
+    //     rollupOptions: {
+    //         external: ["react", "react-dom"],
+    //         output: {
+    //             globals: {
+    //                 react: "React",
+    //             },
+    //         },
+    //     },
+    // },
+    css: {
+        postcss: {
+            plugins: [tailwindcss(), autoprefixer()],
+        },
+    },
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+        },
+    },
+    plugins: [react(), tailwindcss(), libInjectCss()],
+});
